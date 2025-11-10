f6ca0d533890bafe49e02ae4cff9302a72c5a697
Romi <noya.cohen@post.runi.ac.il>
2025-11-10 10:01:07 +0200

    hw2

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,30 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = (args[0]);
+            word = word.toUpperCase();
+            int num = Integer.parseInt(args[1]);
+            int length = word.length();
+
+            
+            for (int i = 0; i < length; i++) {
+                String anOrA;
+                if (word.charAt(i) == 'A' || word.charAt(i) == 'E' || 
+                word.charAt(i) == 'F' || word.charAt(i) == 'H' || 
+                word.charAt(i) == 'I' || word.charAt(i) == 'L' || 
+                word.charAt(i) == 'M' || word.charAt(i) == 'N' || 
+                word.charAt(i) == 'O' || word.charAt(i) == 'R' || 
+                word.charAt(i) == 'S' || word.charAt(i) == 'X'){
+                  anOrA = " an ";   
+                }
+                else {
+                        anOrA = " a ";
+                }
+                System.out.println("Give me" + anOrA + word.charAt(i) + "!");
+            }
+            System.out.println("What does that spell?");
+            for (int i = 0; i < num; i++) {
+                System.out.println(word + "! ! !");
         }
+}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,22 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+    
+        String Time_string = args[0];
+        int new_minuets = Integer.parseInt(args[1]);
+        int hh = Integer.parseInt("" + Time_string.charAt(0) + Time_string.charAt(1));
+        int mm = Integer.parseInt("" + Time_string.charAt(3) + Time_string.charAt(4));
+        int totalMin = (hh*60) + mm + new_minuets;
+        int totalH = totalMin/60;
+        int newH = totalH%24;
+        int newnew = totalMin - (totalH*60);
+        String hourString = String.valueOf(newH);
+        String minuteString = String.valueOf(newnew);
+        if (newnew < 10) {
+           minuteString = "0" + minuteString;
+        }
+        if (newH < 10){
+            hourString = "0" + hourString;
+        }
+         System.out.println(hourString + ":" + minuteString);
     }
 }
